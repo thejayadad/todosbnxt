1870875400d312fb65b094b6c5595aab42d15b55
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-01 13:47:49 -0400

    update

---
+++ studentlog/src/main/java/com/studentlog/studentlog/StudentlogApplication.java
@@ -0,0 +1,13 @@
+package com.studentlog.studentlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentlogApplication.class, args);
+	}
+
+}
---
+++ studentlog/src/test/java/com/studentlog/studentlog/StudentlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentlog.studentlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
